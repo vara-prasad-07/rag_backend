e05630585ec9c482ab88631b5ba4ae18d6965134
vara prasad <varap2684@gmail.com>
2025-07-09 18:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from rag_model import chatbot
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 
 app = FastAPI()
-api=app
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Change to your frontend URL in production
@@ -22,3 +23,9 @@ class QueryRequest(BaseModel):
 def ask_question(request: QueryRequest):
     answer = bot.query(request.query)
     return {"answer": answer}
+    
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Railway!"}
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
